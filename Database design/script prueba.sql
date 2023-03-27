b9d50b3cf39761ab1a9446162bfb19baeddf52f5
Manuel Estrada <74110365+ManuelEsttrada@users.noreply.github.com>
2023-03-27 12:40:15 -0600

    Add files via upload

---
+++ Database design/script prueba.sql
@@ -0,0 +1,13 @@
+/*CREATE DATABASE prueba;*/
+USE prueba;
+
+/*CREATE TABLE DatosPrueba(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+dato INT 
+)COLLATE='utf8_bin';*/
+
+
+/*INSERT INTO DatosPrueba (dato) VALUES (3),(4),(6),(8),(12),(11),(3498),(0),(92);*/
+
+SELECT * FROM DatosPrueba;
+
